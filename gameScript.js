0e75d682cb86d49fae3c6a826c249fb1706ac4f8
00Greenwood <marcus_greenwood@hotmail.co.uk>
2020-05-02 17:05:29 +0100

    Draw a background grid

---
+++ gameScript.js
@@ -0,0 +1,24 @@
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+var w = canvas.width;
+var h = canvas.height;
+
+function drawBackground() {
+    ctx.lineWidth = 0.1;
+    for (x = 0; x <= w; x += 15) {
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, h);
+    }
+    for (y = 0; y <= h; y += 15) {
+        ctx.moveTo(0, y);
+        ctx.lineTo(w, y);
+    }
+    ctx.stroke();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, w, h);
+    drawBackground();
+}
+
+setInterval(draw, 10);
